7ee3f8c426de5bdaf6fad79abf1391cfb6a9dbd3
Prayank verma <prayankv4@gmail.com>
2021-11-17 01:49:45 +0530

    16/11/21

--- app/src/main/java/com/example/miwok/MainActivity.java
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView numberView = (TextView) findViewById(R.id.textView1);
+        TextView familymemberView = (TextView) findViewById(R.id.textView2);
+        TextView colorsView = (TextView) findViewById(R.id.textView3);
+        TextView phrasesView = (TextView) findViewById(R.id.textView4);
+
+
+        numberView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( getApplicationContext(), NumberActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        familymemberView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FamilyMemberActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        colorsView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ColorsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        phrasesView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PhrasesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/example/miwok/NumberActivity.java
+++ app/src/main/java/com/example/miwok/NumberActivity.java
@@ -3,6 +3,11 @@ package com.example.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
+
+       ListView listView= (ListView) findViewById(R.id.numberView);
+
+        String[] words = getResources().getStringArray(R.array.words);
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,words);
+
+        listView.setAdapter(arrayAdapter);
+
+
+
+
+
+
+
+
     }
 }
