adb59300062353ebb876402ba12e5dafb1d7a8ea
Prayank verma <prayankv4@gmail.com>
2021-11-21 03:57:34 +0530

    add audio initial

--- app/src/main/java/com/example/miwok/NumberActivity.java
+++ app/src/main/java/com/example/miwok/NumberActivity.java
@@ -3,10 +3,14 @@ package com.example.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -39,6 +43,62 @@ public class NumberActivity extends AppCompatActivity {
         WordAdapter adapter = new WordAdapter(this,words,R.color.green,R.color.green_lite);
 
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+
+
+
+
+
+                switch (position){
+                    case 0: {
+                       MediaPlayer mediaPlayer  = MediaPlayer.create(NumberActivity.this, R.raw.one);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+
+                        break;}
+                    case 1: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.two);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+                    case 2: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.three);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+                    case 3: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.four);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+                    case 4: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.five);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+                    case 5: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.six);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+                    case 6: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.seven);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+                    case 7: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.eight);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+                    case 8: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.nine);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+                    case 9: {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(NumberActivity.this, R.raw.ten);
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                        break;}
+
+                }
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/miwok/Word.java
+++ app/src/main/java/com/example/miwok/Word.java
@@ -5,11 +5,13 @@ public class Word {
     private final String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1;
+    private  int mAudioResourceId;
 
 
     public Word(String mDefaultTranslation, String mMiwokTranslation) {
         this.mDefaultTranslation = mDefaultTranslation;
         this.mMiwokTranslation = mMiwokTranslation;
+
     }
 
 
